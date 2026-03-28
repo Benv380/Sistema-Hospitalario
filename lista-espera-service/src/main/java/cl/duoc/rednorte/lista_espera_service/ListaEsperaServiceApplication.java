a7409be75ed0d4d0b9c4803a1166294c1a739868
Diego Azcarategui <diego.azcarategui@gmail.com>
2026-03-28 12:02:50 -0300

    Estructura inicial lista-espera

---
+++ lista-espera-service/src/main/java/cl/duoc/rednorte/lista_espera_service/ListaEsperaServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.rednorte.lista_espera_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaEsperaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaEsperaServiceApplication.class, args);
+	}
+
+}
---
+++ lista-espera-service/src/test/java/cl/duoc/rednorte/lista_espera_service/ListaEsperaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.rednorte.lista_espera_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaEsperaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
